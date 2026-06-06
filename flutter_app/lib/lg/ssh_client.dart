1e17bb2575c35619a89588c0dcec3dbb71daced5
KabirKhanuja <kabirkhanuja@gmail.com>
2026-06-06 10:58:29 +0530

    chore: ssh structure

--- flutter_app/lib/lg/ssh_client.dart
+++ flutter_app/lib/lg/ssh_client.dart
@@ -6,23 +6,17 @@ import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SSHConnection {
-  /// SSH client for managing the connection.
   SSHClient? client;
-
-  /// Number of screens in the Liquid Galaxy system, default is 3 for this project as per requirements.
   int screenAmount = 3;
 
-  /// Gets the left screen number.
   int get leftScreen {
     return screenAmount ~/ 2 + 2;
   }
 
-  /// Gets the right screen number.
   int get rightScreen {
     return screenAmount ~/ 2 + 1;
   }
 
-  /// Gets the password from the shared preferences. Returns null if not found.
   Future<String?> get password async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString('lg_password');
@@ -32,7 +26,6 @@ class SSHConnection {
     return client?.username ?? 'lg';
   }
 
-  /// Returns the sftp client
   Future<SftpClient> getSftp() async {
     SftpClient sftp;
     try {
@@ -44,31 +37,22 @@ class SSHConnection {
     return sftp;
   }
 
-  /// Checks if the SSH client is connected.
-  ///
-  /// Returns `true` if connected, `false` otherwise.
   Future<bool> isConnected() async {
     if (client == null || client!.isClosed) return false;
     try {
-      // Attempt to execute a simple command to check the connection status
       final result = await sendCommand('echo "check connection"');
       return result != null;
     } catch (e) {
-      // If an exception occurs, the connection is not active
       return false;
     }
   }
 
-  /// Disconnects the SSH client if connected.
   Future<void> disconnect() async {
     if (await isConnected()) {
       client!.close();
     }
   }
 
-  /// Connects to the Liquid Galaxy system
-  ///
-  /// Returns `true` if the connection is successful, `false` otherwise.
   Future<bool> connect() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
@@ -92,7 +76,6 @@ class SSHConnection {
 
       await client!.authenticated;
 
-      // In a real LG, screen amount is often 3 or 5.
       final screenAmountString = prefs.getString('lg_screen_amount') ?? "3";
       screenAmount = int.parse(screenAmountString);
 
@@ -103,7 +86,6 @@ class SSHConnection {
     return true;
   }
 
-  /// Sends the [command] to the Liquid Galaxy.
   Future<String?> sendCommand(String command) async {
     try {
       if (client == null) return null;
@@ -119,36 +101,28 @@ class SSHConnection {
     }
   }
 
-  /// Handles an SSH channel open error.
   Future<void> handleSSHChannelOpenError() async {
     await disconnect();
     await connect();
   }
 
-  /// Uploads a file to the Liquid Galaxy.
-  ///
-  /// requires the [filePath] of the file to upload.
   Future<void> upload(String filePath) async {
     if (!await isConnected()) {
       return;
     }
 
     try {
-      // Load file data from assets
       final ByteData data = await rootBundle.load(filePath);
 
-      // Extract the file name from the provided filePath
       final fileName = filePath.split('/').last;
 
       final sftp = await getSftp();
 
-      // Upload file directly from byte data
       final remoteFile = await sftp.open(
         '/var/www/html/$fileName',
         mode: SftpFileOpenMode.create | SftpFileOpenMode.write,
       );
 
-      // Convert ByteData to Uint8List and write it directly
       final uint8ListData = data.buffer.asUint8List();
       await remoteFile.write(Stream.value(uint8ListData).cast<Uint8List>());
       await remoteFile.close();
@@ -157,9 +131,6 @@ class SSHConnection {
     }
   }
 
-  /// Sends a KML file to the Liquid Galaxy system.
-  ///
-  /// [kml] is the KML content to send.
   Future<void> sendKml(String kml, {List<String> images = const []}) async {
     if (!await isConnected()) {
       return;
@@ -185,7 +156,6 @@ class SSHConnection {
       await remoteFile.write(Stream.value(kmlBytes).cast<Uint8List>());
       await remoteFile.close();
 
-      // Get the IP of the master to construct the URL for slaves to fetch
       SharedPreferences prefs = await SharedPreferences.getInstance();
       final masterIp = prefs.getString('lg_ip') ?? 'localhost';
 
@@ -197,10 +167,6 @@ class SSHConnection {
     }
   }
 
-  /// Sends a KML file to a specific slave screen.
-  ///
-  /// [screenNumber] is the screen number.
-  /// [kml] is the KML content to send.
   Future<void> sendKMLToSlave(int screenNumber, String kml) async {
     if (await isConnected() == false) {
       return;
